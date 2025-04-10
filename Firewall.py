08d1bb114d0ac6a0339eb5b03c9ba5ba05375ab2
Noah-Vining <ncvining@ualr.edu>
2025-04-09 22:43:45 -0500

    Progress on Blocking and Unblocking
    Almost finished with blocking and unblocking

--- Firewall.py
+++ Firewall.py
@@ -5,6 +5,7 @@ import time
 from collections import defaultdict
 from scapy.all import *
 from scapy.layers.inet import IP
+import ctypes
 
 # Firewall Project
 
@@ -17,6 +18,20 @@ from scapy.layers.inet import IP
 - DDOS Protection
 
 """
+
+# These don't work yet!! Incorrect rule being sent to system?
+def ip_block(ip):
+    print("Made it here!")
+    os.system(f'netsh advfirewall firewall delete rule name="BlockIP-{ip}"')
+    print(ip, " is blocked!")
+
+
+def ip_unblock(ip):
+    # Sends a command to block IP on Windows Computer
+    os.system(f'netsh advfirewall firewall add rule name="BlockIP-{ip}" dir=out action=block remoteip={ip}')
+    print(ip, " is unblocked!")
+
+
 # Completely Unfinished
 def firewall(current_packet):
     # Grabs IP from packet
@@ -27,44 +42,52 @@ def firewall(current_packet):
     if ip in wlist_ips:
         return
 
+    # Blocks IP if it is in blocklist
     if ip in blist_ips:
-        print(ip, " is blocked")
+       ip_block(ip)
 
 
 
 if __name__ == "__main__":
-    # Ability to add IPs to blacklist's manually
+    #Checks if program is running with needed admin privileges
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        print("Admin Privileges required to run program!")
+        sys.exit(1)
+
+    # Ability to add IPs to blacklist and whitelist manually
     add_ip = 'b'
     while add_ip != 'y' and add_ip != 'n':
-        add_ip = input("Would you like to enter or remove IP's to blacklist manually? (Y/N) ")
+        add_ip = input("Would you like to enter IP's to blacklist or whitelist manually? (Y/N) ")
         add_ip = add_ip.lower()
 
     if add_ip == "y":
         choice = 0
         while choice != 1 and choice != 2:
-            choice = input("\nWhich would you like to do?\n1. Add\n2. Remove\n")
+            choice = input("Which would you like to add to?\n1. Whitelist\n2. Blacklist\n")
             choice = int(choice)
 
         new_ip = input("\nPlease enter IP: ")
 
         if choice == 1:
+            wlist = open('whitelist.txt', 'a')
+            wlist.write(new_ip)
+            wlist.write('\n')
+            wlist.close()
+
+        elif choice == 2:
             blist = open('blacklist.txt', 'a')
             blist.write(new_ip)
             blist.write('\n')
             blist.close()
 
-        if choice == 2:
-            # We can decide if we want this functionality or not, doesn't work yet
-            blist = open('blacklist.txt', 'a')
-            blist.close()
-
+        print("IP Added!")
 
     # Create set for whitelist and blacklist
     wlist = open('whitelist.txt', 'r')
-    wlist_ips = wlist.readlines()
+    wlist_ips = wlist.read().splitlines()
 
     blist = open('blacklist.txt', 'r')
-    blist_ips = blist.readlines()
+    blist_ips = blist.read().splitlines()
 
     # Grabs IP and sends it's packet to firewall function
     print("Detecting IP's...")
